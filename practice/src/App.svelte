192af734430d4a467f613e244964137388b688e1
xhermitx <primary.rohit@gmail.com>
2024-08-13 20:57:46 +0530

    simple components

---
+++ practice/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Counter from './lib/Counter.svelte'
+  const name = 'Svelte'
+</script>
+
+<h1>Hello {name}!</h1>
+<Counter initialCount={3} maxCount={10}/>
+
+<style>
+
+</style>
---
+++ practice/src/lib/Counter.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let initialCount, maxCount;
+
+  let count = initialCount;
+  $: msg = `You have clicked ${count} times`;
+  
+  function increment(){
+    if(count === maxCount) return;
+    count++;
+  }
+</script>
+
+<button on:click={increment}>
+  Clicks: {count}
+</button>
+
+<h3>{msg}</h3>
